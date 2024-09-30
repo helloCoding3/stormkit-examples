902a5a7c2246e8d42f27c0aa0b64c2f7dd5c9c48
helloCoding3 <joe_young1@163.com>
2024-09-30 20:24:27 +0800

    测试3

--- api/server.ts
+++ api/server.ts
@@ -1,10 +1,15 @@
 import * as http from "node:http";
-
 // Using object-return syntax
 export default (req: http.IncomingMessage) => {
   const obj = {
     name: "张三",
     age: 18,
   };
-  return obj;
+  return {
+    body: obj,
+    headers: {
+      "X-Custom-Header": "Sample Project",
+    },
+    status: 200,
+  };
 };
