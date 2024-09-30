701977eaab32dc79a476db30cf6d3e9056fd0d87
helloCoding3 <joe_young1@163.com>
2024-09-30 20:37:40 +0800

    测试4

--- api/server.ts
+++ api/server.ts
@@ -1,15 +1,38 @@
 import * as http from "node:http";
-// Using object-return syntax
-export default (req: http.IncomingMessage) => {
-  const obj = {
-    name: "张三",
-    age: 18,
-  };
-  return {
-    body: obj,
-    headers: {
-      "X-Custom-Header": "Sample Project",
-    },
-    status: 200,
-  };
+import fetch from "node-fetch";
+
+// Using standard Node.js API
+export default (req: http.IncomingMessage, res: http.ServerResponse) => {
+  console.log("sample log from sample-project");
+
+  fetch("https://api.stormkit.io", {
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: null,
+    method: "GET",
+  })
+    .then(async (data) => {
+      const text = await data.text();
+
+      res.setHeader("Content-Type", "application/json");
+      res.write(
+        JSON.stringify({
+          apiVersion: text.split("Version: ")[1].split(/\s/)[0],
+        })
+      );
+
+      res.end();
+    })
+    .catch((e) => {
+      console.log(e);
+
+      res.setHeader("Content-Type", "application/json");
+      res.write(
+        JSON.stringify({
+          error:
+            "Something went wrong while fetching api version. Check the runtime logs for more information.",
+        })
+      );
+
+      res.end();
+    });
 };
